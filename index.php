83abd7dbbd38ab38ce017796df11a1d7d55d7f24
Freelancer ujjal <52934027+Freelancer-U-B@users.noreply.github.com>
2019-07-22 18:40:18 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP-TEST</title>
+	<?php
+	include 'insert.php';
+	?>
+</head>
+<body>
+	<a href="index.php">BACK</a>
+<form method="POST" type="" action="">
+	<span>NAME</span>
+	<input type="text" name="name">
+	<span>EMAIL</span>
+	<input type="email" name="email">
+	<span>MOBILE NUMBER</span>
+	<input type="text" name="mobile">
+	<input type="submit" name="b1">
+</form>
+</body>
+</html>
